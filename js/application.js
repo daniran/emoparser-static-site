eff2409c722d2c09dc01d932ee26afc103b6cb87
Danny Rankevich <danny.ran@gmail.com>
2014-04-16 02:01:27 +0300

    site

--- js/application.js
+++ js/application.js
@@ -1,1 +1,44 @@
-/* lol */
+$(function() {
+
+    var parser = new EmoticonsParser({
+        emoticons: {
+
+            smile: {
+                emos: [':)', ':-)']
+            },
+
+            surprised: {
+                emos: [':-O', ':-o)']
+            },
+
+            wink: {
+                emos: [';)', ';-)']
+            },
+
+            confused: {
+                emos: [':-S', ':-s)']
+            },
+
+            crying: {
+                emos: [":'("]
+            },
+
+            h: {
+                emos: [':)', ':-)']
+            },
+
+            a: {
+                emos: [':)', ':-)']
+            }
+        },
+        emoticon_html: '<span class="emo-class emoticon-{EMOTICON}" title="{EMOTICON}"></span>'
+    });
+
+
+    $('#test-button').click(function() {
+        var value = $('#text-input').val();
+        var resultElem = $('#result');
+        var parseResult = parser.parseText(value)
+        resultElem.html(parseResult.replace(/\n/g, '<br />'));
+    });
+});
